f1187efb363d2e8e91d89364725eb081b7858b94
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-18 22:35:06 +0200

    installing socket.io, configure it with our express server, adding a form and implemented code to send messages from the client and server emit that message to all the connected clients

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +13,16 @@ const puplicDirectoryPath = path.join(process.cwd(), './puplic');
 
 app.use(express.static(puplicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('new Websocket connection ');
+
+	socket.emit('message', 'Welcome');
+
+	socket.on('sendMessage', (message) => {
+		io.emit('message', message);
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Server is up and running on port: ${port}`);
 });
