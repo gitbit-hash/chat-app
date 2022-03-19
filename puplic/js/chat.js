101c753c20dfea1b561e20edd475fbb454477a41
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-19 15:42:31 +0200

    setting up form and buttons states

--- puplic/js/chat.js
+++ puplic/js/chat.js
@@ -1,15 +1,29 @@
 const socket = io();
 
+//Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
 	console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	//Disable
+	$messageFormButton.setAttribute('disabled', 'disabled');
 
 	const message = e.target.elements.message.value;
 
 	socket.emit('sendMessage', message, (error) => {
+		//Enable
+		$messageFormButton.removeAttribute('disabled');
+		$messageInput.value = '';
+		$messageInput.focus();
+
 		if (error) {
 			return console.log(error);
 		}
@@ -23,9 +37,14 @@ document.querySelector('#send-location').addEventListener('click', () => {
 		return alert("Sorry, your browser doesn't support Geolocation");
 	}
 
+	//Disable button
+	$sendLocationButton.setAttribute('disabled', 'disabled');
+
 	navigator.geolocation.getCurrentPosition(
 		({ coords: { latitude, longitude } }) => {
 			socket.emit('sendLocation', { latitude, longitude }, () => {
+				$sendLocationButton.removeAttribute('disabled');
+
 				console.log('Location shared');
 			});
 		}
