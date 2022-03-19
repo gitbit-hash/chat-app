44fea12e260f6c75a907977de442e8373d71666a
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-19 14:29:37 +0200

    adding the functionality to get the geolocation and sharing the location

--- puplic/js/chat.js
+++ puplic/js/chat.js
@@ -11,3 +11,15 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
 	socket.emit('sendMessage', message);
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return alert("Sorry, your browser doesn't support Geolocation");
+	}
+
+	navigator.geolocation.getCurrentPosition(
+		({ coords: { latitude, longitude } }) => {
+			socket.emit('sendLocation', { latitude, longitude });
+		}
+	);
+});
