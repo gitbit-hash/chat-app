052d7598bb54979952459aa9c76b15b2f2673ffa
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-19 17:38:04 +0200

    adding a formatted timestamp when a client send his geolocation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
 const Filter = require('bad-words');
-const { generateMessage } = require('./utils/messages');
+const {
+	generateMessage,
+	generateLocationMessage,
+} = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -35,7 +38,9 @@ io.on('connection', (socket) => {
 	socket.on('sendLocation', (location, callback) => {
 		io.emit(
 			'locationMessage',
-			`https://google.com/maps?q=${location.latitude},${location.longitude}`
+			generateLocationMessage(
+				`https://google.com/maps?q=${location.latitude},${location.longitude}`
+			)
 		);
 		callback();
 	});
