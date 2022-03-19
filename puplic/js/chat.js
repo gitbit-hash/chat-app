0d8a0767b597f47e6b5e83b62678991fe19846f4
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-19 15:15:22 +0200

    setting up client acknowledgement function and server with bad-words library so the client cannot send bad words, setting up client acknowledgment function and server when the client share location

--- puplic/js/chat.js
+++ puplic/js/chat.js
@@ -9,7 +9,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
 	const message = e.target.elements.message.value;
 
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error);
+		}
+
+		console.log('Message delivered!');
+	});
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -19,7 +25,9 @@ document.querySelector('#send-location').addEventListener('click', () => {
 
 	navigator.geolocation.getCurrentPosition(
 		({ coords: { latitude, longitude } }) => {
-			socket.emit('sendLocation', { latitude, longitude });
+			socket.emit('sendLocation', { latitude, longitude }, () => {
+				console.log('Location shared');
+			});
 		}
 	);
 });
